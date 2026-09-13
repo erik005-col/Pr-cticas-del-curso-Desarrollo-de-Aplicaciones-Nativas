8c381a87fff1ca5d1304d23ee49ebfa4397369e4
Erik Ortiz Leal <ortizerik694@gmail.com>
2026-09-13 11:22:29 -0700

    Practica 1 con Jetpack Compose y navegacion entre pantallas

---
+++ app/src/main/java/com/uas/practica01/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.uas.practica01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.uas.practica01.ui.theme.Practica01Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica01Theme {
+                AppNavigation()
+            }
+        }
+    }
+}
