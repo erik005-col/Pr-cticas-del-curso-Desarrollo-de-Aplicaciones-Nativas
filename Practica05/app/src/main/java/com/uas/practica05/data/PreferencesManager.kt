65abcdc4dd8cd83f68e8c1942a064f0d6dfdde7e
Erik Ortiz Leal <ortizerik694@gmail.com>
2026-09-25 18:40:42 -0700

    Guardando practica05 localmente

---
+++ Practica05/app/src/main/java/com/uas/practica05/data/PreferencesManager.kt
@@ -0,0 +1,42 @@
+package com.uas.practica05.data
+
+
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesManager(context: Context) {
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("UserPreferences", Context.MODE_PRIVATE)
+
+    companion object {
+        const val KEY_USERNAME = "key_username"
+        const val KEY_NOTIFICATIONS = "key_notifications"
+        const val KEY_DARK_THEME = "key_dark_theme"
+    }
+
+    fun saveSettings(username: String, notifications: Boolean, darkTheme: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putString(KEY_USERNAME, username)
+        editor.putBoolean(KEY_NOTIFICATIONS, notifications)
+        editor.putBoolean(KEY_DARK_THEME, darkTheme)
+        editor.apply()
+    }
+
+    fun getUsername(): String {
+        return sharedPreferences.getString(KEY_USERNAME, "") ?: ""
+    }
+
+    fun getNotifications(): Boolean {
+        return sharedPreferences.getBoolean(KEY_NOTIFICATIONS, false)
+    }
+
+    fun getDarkTheme(): Boolean {
+        return sharedPreferences.getBoolean(KEY_DARK_THEME, false)
+    }
+
+    fun clearPreferences() {
+        sharedPreferences.edit().clear().apply()
+    }
+}
